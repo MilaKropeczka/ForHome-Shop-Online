d40d737c6a8d81b16fd4f8fc289586ae4b059512
MilaKropeczka <milakropeczka@gmail.com>
2022-08-10 10:40:35 +0200

    Add Link Home to MobileNav

--- src/components/Nav/PopUpMobile/PopUpMobile.js
+++ src/components/Nav/PopUpMobile/PopUpMobile.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import styles from './PopUpMobile.module.sass';
 import Item from './Item/Item';
+import { Link } from 'react-router-dom';
 
 function PopUpMobile() {
 	return (
 		<div className='d-flex justify-content-center align-items-center'>
 			<ul className={styles.lists}>
+				<li>
+					<Link to={``} className='secondaryFont'>
+						Strona Główna
+					</Link>
+				</li>
 				<Item title='Sypialnia' className='secondaryFont' />
 				<Item title='Salon' className='secondaryFont' />
 				<Item title='Kuchnia' className='secondaryFont' />
