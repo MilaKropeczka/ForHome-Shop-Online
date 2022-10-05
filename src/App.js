a696bd89995345395a35df83d214114a7078eb71
MilaKropeczka <milakropeczka@gmail.com>
2022-10-05 12:04:01 +0200

    Add: Limit amount cart

--- src/App.js
+++ src/App.js
@@ -248,18 +248,21 @@ function App() {
 		let updateItems = [...mapItems];
 		let editItems = updateItems[e.target.getAttribute('name')];
 		let nameItems = updateItems[e.target.getAttribute('name')].id;
-		editItems.amount = editItems.amount + 1;
+		if (editItems.amount < 9) {
+			editItems.amount = editItems.amount + 1;
+		}
 		updateItems.splice(nameItems, 1, editItems);
 		setMapItems(updateItems);
 	};
 
 	const removeAmount = function (e) {
 		e.preventDefault();
-
 		let updateItems = [...mapItems];
 		let editItems = updateItems[e.target.getAttribute('name')];
 		let nameItems = updateItems[e.target.getAttribute('name')].id;
-		editItems.amount = editItems.amount - 1;
+		if (editItems.amount > 1) {
+			editItems.amount = editItems.amount - 1;
+		}
 		updateItems.splice(nameItems, 1, editItems);
 		setMapItems(updateItems);
 	};
